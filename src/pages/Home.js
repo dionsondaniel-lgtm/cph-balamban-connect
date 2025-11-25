d6ef0e28e2786de2436ebfa000d12b4be821e724
Daniel Dionson <dionsondaniel@gmail.com>
2025-11-25 23:00:06 +0800

    CPH Balamban Connect 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import "./Home.css";
 
 export default function Home() {
   const navigate = useNavigate();
